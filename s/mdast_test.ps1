cebedffd65f4efb99c2afddb1d42e407c7ff331f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-02-06 19:07:06 -0800

    tweak test scripts

--- s/mdast_test.ps1
+++ s/mdast_test.ps1
@@ -1,1 +1,2 @@
-node .\mdfmt\astdump.js '.\books\csharp-language_fmt\0010-getting-started-with-csharp-language\060 Creating a new project using Xamarin Studio.md'
+#node ./mdfmt/astdump.js './books/csharp-language_fmt/0010-getting-started-with-csharp-language/060 Creating a new project using Xamarin Studio.md'
+node ./mdfmt/astdump.js './books/csharp-language_fmt/0010-getting-started-with-csharp-language/010 Creating a new console application Visual Studio.md'
--- s/mdfmt_test.ps1
+++ s/mdfmt_test.ps1
@@ -1,1 +1,2 @@
-node ./mdfmt/mdfmt.js './books/csharp-language_fmt/0010-getting-started-with-csharp-language/060 Creating a new project using Xamarin Studio.md'
+#node ./mdfmt/mdfmt.js './books/csharp-language_fmt/0010-getting-started-with-csharp-language/060 Creating a new project using Xamarin Studio.md'
+node ./mdfmt/mdfmt.js './books/csharp-language_fmt/0010-getting-started-with-csharp-language/010 Creating a new console application Visual Studio.md'
