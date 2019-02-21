78f04290fd1f2365a9bbce3a97f7e26d1d62114a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-02-20 22:19:07 -0800

    add cpp/ios redownload scripts

---
+++ s/redownload/ios.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
+
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
+go build -o ./gen.exe ./cmd/gen-books
+exitIfFailed
+
+./gen.exe -redownload-one 3626edc1bd044431afddc89648a7050f
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
