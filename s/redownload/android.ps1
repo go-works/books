cea4c1e3c06fac274723e0b593807a8c81c87fee
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-03-04 23:53:52 -0800

    update android from notion

--- s/redownload/android.ps1
+++ s/redownload/android.ps1
@@ -1,4 +1,4 @@
- #!/usr/bin/env pwsh
+#!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
@@ -10,3 +10,4 @@ exitIfFailed
 
 ./gen.exe -redownload-book f90b0a6b648343e28dc5ed6e8f5c0780
 Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
