5164989cd0c6c8f3e07a5aa8070b4a4f8f81c503
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-03-06 21:09:33 -0800

    download bash from notion

---
+++ s/redownload/algorithms.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
+
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
+go build -o ./gen.exe ./cmd/gen-books
+exitIfFailed
+
+./gen.exe -redownload-book 039ec42ee62f412e983e6d5b6b201b60
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
---
+++ s/redownload/bash.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
+
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
+go build -o ./gen.exe ./cmd/gen-books
+exitIfFailed
+
+./gen.exe -redownload-book 77d28932012b489db9a6d0b349cea865
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
