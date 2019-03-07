fb42055808caa3636383f235abcd24ff96d8e84e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-03-07 02:13:58 -0800

    add .net framework

---
+++ s/redownload/netframework.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
+
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
+go build -o ./gen.exe ./cmd/gen-books
+exitIfFailed
+
+./gen.exe -redownload-book 6289ba4610874393844dfbae890ed035
+Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
+
