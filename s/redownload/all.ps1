cb8cdaa0fd8b631b2d462df2238be1475348e1a2
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-05-04 14:46:26 -0700

    update from notion

--- s/redownload/all.ps1
+++ s/redownload/all.ps1
@@ -8,5 +8,5 @@ Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
 go build -o ./gen.exe
 exitIfFailed
 
-./gen.exe -no-cache -preview
+./gen.exe -all-books
 Remove-Item -Force -ErrorAction SilentlyContinue ./gen.exe
