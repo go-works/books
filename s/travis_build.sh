1dff2569eb44f096afd7eb03e519c47fcc1696e6
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-08-26 10:41:19 -0700

    don't deploy from travis

--- s/travis_build.sh
+++ s/travis_build.sh
@@ -6,9 +6,9 @@ go build -o gen-books
 
 ./gen-books -no-update-output -analytics UA-113489735-1
 
-if [ -z ${NETLIFY_TOKEN+x} ]
-then
-    echo "Skipping upload because NETLIFY_TOKEN not set"
-else
-    netlify deploy --prod --dir=www --site=7df32685-1421-41cf-937a-a92fde6725f4 --auth=${NETLIFY_TOKEN}
-fi
+#if [ -z ${NETLIFY_TOKEN+x} ]
+#then
+#    echo "Skipping upload because NETLIFY_TOKEN not set"
+#else
+#    netlify deploy --prod --dir=www --site=7df32685-1421-41cf-937a-a92fde6725f4 --auth=${NETLIFY_TOKEN}
+#fi
