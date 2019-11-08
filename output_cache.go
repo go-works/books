ae114fee3916852e5e772a3711356f09de7f6a1a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-11-08 14:04:11 -0800

    remove unused code

--- output_cache.go
+++ output_cache.go
@@ -3,76 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
-	"os/exec"
-	"path/filepath"
 	"strings"
 
-	"github.com/google/shlex"
 	"github.com/kjk/u"
 )
-
-// runs `go run ${path}` and returns captured output`
-func getGoOutput(path string) (string, error) {
-	dir, fileName := filepath.Split(path)
-	cmd := exec.Command("go", "run", fileName)
-	cmd.Dir = dir
-	out, err := cmd.CombinedOutput()
-	return string(out), err
-}
-
-func getRunCmdOutput(path string, runCmd string) (string, error) {
-	parts, err := shlex.Split(runCmd)
-	maybePanicIfErr(err)
-	if err != nil {
-		return "", err
-	}
-	exeName := parts[0]
-	parts = parts[1:]
-	var parts2 []string
-	srcDir, srcFileName := filepath.Split(path)
-
-	// remove empty lines and replace variables
-	for _, part := range parts {
-		if len(part) == 0 {
-			continue
-		}
-		switch part {
-		case "$file":
-			part = srcFileName
-		}
-		parts2 = append(parts2, part)
-	}
-	//fmt.Printf("getRunCmdOutput: running '%s' with args '%#v'\n", exeName, parts2)
-	cmd := exec.Command(exeName, parts2...)
-	cmd.Dir = srcDir
-	out, err := cmd.CombinedOutput()
-	//fmt.Printf("getRunCmdOutput: out:\n%s\n", string(out))
-	return string(out), err
-}
-
-func stripCurrentPathFromOutput(s string) string {
-	path, err := filepath.Abs(".")
-	u.PanicIfErr(err)
-	return strings.Replace(s, path, "", -1)
-}
-
-// it executes a code file and captures the output
-// optional runCmd says
-func getOutput(path string, runCmd string) (string, error) {
-	if runCmd != "" {
-		//fmt.Printf("Found :run cmd '%s' in '%s'\n", runCmd, path)
-		s, err := getRunCmdOutput(path, runCmd)
-		return stripCurrentPathFromOutput(s), err
-	}
-
-	// do default
-	ext := strings.ToLower(filepath.Ext(path))
-	if ext == ".go" {
-		s, err := getGoOutput(path)
-		return stripCurrentPathFromOutput(s), err
-	}
-	return "", fmt.Errorf("getOutput(%s): files with extension '%s' are not supported", path, ext)
-}
 
 var allowedLanguages = map[string]bool{
 	"go":         true,
@@ -201,26 +135,5 @@ func getOutputCached(cache *Cache, sf *SourceFile) error {
 		err = updateGlotID(cache, sf)
 		must(err)
 	}
-
-	/*
-		path := sf.Path
-		ext := strings.ToLower(filepath.Ext(path))
-		switch ext {
-		case ".json", ".csv", ".yml", ".xml":
-			return nil
-		}
-
-		// fmt.Printf("loadFileCached('%s') failed with '%s'\n", outputPath, err)
-		s, err := getOutput(path, sf.Directive.RunCmd)
-		if err != nil {
-			if !sf.Directive.AllowError {
-				logf("getOutput('%s'), got error:\n%s\n", path, s)
-				return err
-			}
-			err = nil
-		}
-		sf.Output = s
-		fmt.Printf("Got output (%d bytes) for '%s' by running locally\n", len(sf.Output), path)
-	*/
 	return nil
 }
